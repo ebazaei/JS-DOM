f6f5c522ed8fd6e94ce42972f81377e4e60def28
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-22 09:37:30 +0100

    test-1

---
+++ script.js
@@ -0,0 +1,11 @@
+// const body = document.querySelector('body'); // you can also use 'document.body'
+// const newParagraph = document.createElement('p');
+// newParagraph.innerText = 'This paragraph will be added to the body!';
+// newParagraph.style.background = 'red';
+// body.appendChild(newParagraph);
+
+
+const body = document.querySelector('body');
+body.addEventListener('click', function() {
+  body.style.background = 'black';
+});
